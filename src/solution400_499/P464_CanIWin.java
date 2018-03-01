d5f9eb828b5ab5e81959d31f20a6a38b6a97d9c4
zjw245 <zjw131213@163.com>
2018-03-01 12:35:09 +0800

    Problem P464 solved

--- src/solution400_499/P464_CanIWin.java
+++ src/solution400_499/P464_CanIWin.java
@@ -1,9 +1,33 @@
 package solution400_499;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by zjw on 2018/02/03 14:12
  * Description:
  */
 public class P464_CanIWin {
+
+    // can be solved using brute-force, the key is to avoid repeatedly solving sub-problems
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if(desiredTotal <= 0)  return true;
+        if(desiredTotal > (maxChoosableInteger + 1) * maxChoosableInteger / 2) return false;
+        return helper(maxChoosableInteger, desiredTotal, 0, new HashMap<>());
+    }
+
+    private boolean helper(int maxInteger, int total, int state, Map<Integer, Boolean> map) {
+        if(map.containsKey(state))  return map.get(state);
+        for(int i = 0; i < maxInteger; i++) {
+            if((1 << i & state) == 0) {
+                if(total <= i + 1 || !helper(maxInteger, total - (i + 1), (1 << i | state), map)) {
+                    map.put(state, true);
+                    return true;
+                }
+            }
+        }
+        map.put(state, false);
+        return false;
+    }
 
 }
